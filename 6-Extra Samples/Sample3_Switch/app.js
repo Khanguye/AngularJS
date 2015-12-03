1e4505cc24da634a8c8430fa718d2c40494109cf
KhaNguyen <trongkha@yahoo.com>
2015-12-02 21:28:54 -0800

    Add ngShow ngHide ngSwitch
    More samples

---
+++ 6-Extra Samples/Sample3_Switch/app.js
@@ -0,0 +1,29 @@
+var app= angular.module("myApp",['ngAnimate']);
+app.controller("myCtrl",function($scope,$http){
+    $http.get("http://www.filltext.com/?rows=100&id={index}&email={email}&username={username}&password={randomString|5}&pretty=true").
+    success(function(data,status,headers,config){
+        $scope.users=data;
+    }).
+    error(function(data,status,headers,config){
+        $scope.error="Cannot retrieve data";
+    });
+});
+/* option replace ng-include */
+app.directive('includeReplace', function () {
+    return {
+        require: 'ngInclude',
+        restrict: 'A', /* optional */
+        link: function (scope, el, attrs) {
+            el.replaceWith(el.children());
+        }
+    };
+});
+/*
+$http.get
+$http.head
+$http.post
+$http.put
+$http.delete
+$http.jsonp ==> URL?callback=JSON_CALLBACK
+$http.patch
+*/
